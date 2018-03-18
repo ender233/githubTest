81eb5680d0250145b4be126e2c61babc460074eb
ender.liu@outlook.com <ender.liu@outlook.com>
2018-03-19 00:02:55 +0800

    check clion

---
+++ cmake-build-debug_check/CMakeFiles/AsyncLogger.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AsyncLogger.dir/main.cpp.o"
+  "CMakeFiles/AsyncLogger.dir/Logger.cpp.o"
+  "AsyncLogger.pdb"
+  "AsyncLogger"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AsyncLogger.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
